881a8416067df232b552ec62f935a949cba438b6
mubarak6969 <mubarak7842a@gmail.com>
2026-08-13 07:18:30 +0530

    Level 2.2 - HttpResponse with HTML and navigation!

--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,32 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Assalamu Alaikum! My first Django view is working! 🎉")
+    html = """
+    <html>
+        <body style="font-family: Arial; text-align: center; 
+                     background-color: #f0f8ff; padding: 50px;">
+            <h1 style="color: #2e86de;">🕌 Assalamu Alaikum!</h1>
+            <h2>Welcome to My Django Website</h2>
+            <p>I built this with <strong>Python</strong> 
+               and <strong>Django 5.2</strong>!</p>
+            <a href="/about/">Go to About Page →</a>
+        </body>
+    </html>
+    """
+    return HttpResponse(html)
+
 
 def about(request):
-    return HttpResponse("This is the About page! I am Mubarak, a Django developer!")
+    html = """
+    <html>
+        <body style="font-family: Arial; text-align: center;
+                     background-color: #fff9f0; padding: 50px;">
+            <h1 style="color: #e67e22;">👨‍💻 About Me</h1>
+            <p>My name is <strong>Mubarak</strong>.</p>
+            <p>I am learning Django and building real websites!</p>
+            <p>I am a Django developer! 🚀</p>
+            <a href="/">← Back to Home</a>
+        </body>
+    </html>
+    """
+    return HttpResponse(html)
